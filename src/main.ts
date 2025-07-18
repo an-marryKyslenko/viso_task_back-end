637c73baa3d7029d49d686726073356397c54545
an-marryKyslenko <olih.a@icloud.com>
2025-07-18 14:50:18 +0300

    add trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  
   app.use(cookieParser());
   app.enableCors({
     origin: process.env.CLIENT_SITE || 'http://localhost:3000',
     credentials: true
   })
 
-  // app.set('trust proxy', 1);
+  app.set('trust proxy', 1);
 
 
   await app.listen(process.env.PORT ?? 3001);
